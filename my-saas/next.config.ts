d1949a9c2182865091d4d517cb913d31d97214ca
Osajele Odegua <odeguaj@gmail.com>
2025-12-18 08:21:35 +0100

    improved on the redirecting for homepage

--- my-saas/next.config.ts
+++ my-saas/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+//import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // This makes it a 308 redirect (good for SEO)
+      },
+    ]
+  },
 };
 
 export default nextConfig;
